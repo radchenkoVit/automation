9d74c649deb1f89579584268ac3c5f61751786d8
intracomof@gmail.com <intracomof@gmail.com>
2019-05-02 21:43:44 +0300

    add TestRunner

---
+++ src/test/java/com/skillup/automation/TestRunner.java
@@ -0,0 +1,44 @@
+package com.skillup.automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class TestRunner {
+    protected WebDriver driver = null;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before suite");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Before class");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before method");
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After method");
+        driver.quit();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("After class");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After suite");
+    }
+
+}
